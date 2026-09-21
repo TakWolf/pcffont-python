e51da9b0b1cd202264a9de22091f834a2620bd5f
TakWolf <takwolf@foxmail.com>
2026-09-21 15:33:45 +0800

    调整路径解析方式

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-PROJECT_ROOT_DIR = Path(__file__).parent.joinpath('..').resolve()
+PROJECT_ROOT_DIR = Path(__file__).parents[1]
 ASSETS_DIR = PROJECT_ROOT_DIR.joinpath('assets')
 BUILD_DIR = PROJECT_ROOT_DIR.joinpath('build')
